4737def520f5e63eea7fad08b71d3fb0ccffb2da
Ricky Gobby <ricky@Rickys-MacBook-Pro.local>
2021-03-31 10:02:19 +0800

    ugly looking quiz but it runs and does it's job

--- script.js
+++ script.js
@@ -5,21 +5,29 @@ $( document ).ready(function() {
     var finished
     var question = 1;
     var score = 0;
+    // var highscores = localStorage.getItem("a")
+    var i
+    // localStorage.clear();
+
+    for (i = 0; i < localStorage.length; i++)  console.log( localStorage.highscores(i) +" has value " + localStorage[localStorage.highscores(i)] )
 
     $("#start").click(function (e) { 
         e.preventDefault();
+        highscoreList.innerHTML = "";
         score = 0;
-        quizTime = 90;
+        quizTime = 10;
+        question = 1;
         finished = false;
         $("#score").text(score);
         document.getElementById("start").style.display = "none";
         x = setInterval(function() {
             quizTime = quizTime -1;
-            if (quizTime < 0) {
+            if (quizTime <= 0) {
                 quizTime = 0;
             }
+            
             $("#timer").html(quizTime);
-            console.log(quizTime);
+            // console.log(quizTime);
             checkFinish();
             }, 1000);
             document.getElementById("q1").style.display = "block";
@@ -29,6 +37,10 @@ $( document ).ready(function() {
         if (quizTime <= 0 || finished == true) {
             clearInterval(x);
             document.getElementById("start").style.display = "block";
+            for (i = 1; i < 6; i++) {
+                document.getElementById(`q${i}`).style.display = "none";
+            }
+            end()
         }
     }
 
@@ -37,41 +49,77 @@ $("ul").click(function (e) {
     var element = e.target;
     var answer = element.getAttribute("data-answer");
     
-    // document.getElementById(`q${question}`).style.display = "none";
-
-      if (answer === "correct") {
+    if (answer === "correct") {
         $("#response").html("correct");
         score = score + 10
-      } else {
-        // Change the attributes back to their non-animated values
+    } else {
         $("#response").html("wrong");
         if (quizTime <= 5) {
             quizTime = quizTime - quizTime;
         } else {
             quizTime = quizTime - 5;
         }
-      }
-      document.getElementById(`q${question}`).style.display = "none";
-      question = question + 1;
-      $("#score").text(score);
-      if (question == 6) {
-          finished = true;
-          question = 1;
-          scoreBonus();
-      } else {
-      document.getElementById(`q${question}`).style.display = "block";}
+    }
+    document.getElementById(`q${question}`).style.display = "none";
+    question = question + 1;
+    $("#score").text(score);
+    if (question == 6) {
+        end();
+    } else {
+        document.getElementById(`q${question}`).style.display = "block";}
     });
 
     function scoreBonus() {
-        timeBonus = quizTime / 5;
+        timeBonus = Math.ceil(quizTime / 5);
         score = timeBonus + score;
         $("#score").text(score);
     }
-      
+    var highscoreList = document.querySelector("ol");
+    function highscorePage() {
+        highscoreList.innerHTML = "";
+        var i;
+        for (i = 1; i <= localStorage.length; i++) { 
+            console.log(localStorage[i]);
+            var li = document.createElement("li");
+            li.innerText = localStorage[i];
+            highscoreList.appendChild(li);
+        }
 
+    }
+    var clear = document.createElement("BUTTON");
+    $("#highscore").click(function (e) { 
+        e.preventDefault();
+        highscorePage();
+        // var clear = document.createElement("BUTTON");
+        clear.innerHTML = "clear";
+        highscoreList.appendChild(clear);
+    });
 
+        $(clear).click(function (e) { 
+            e.preventDefault();
+            highscoreList.innerHTML = "";
+            localStorage.clear();
+            console.log("cleared")
+        });
 
+        if (finished == true) {
+            for (i = 0; i < 6; i++) {
+                document.getElementById(`q${i}`).style.display = "none";
+            }
+            question = 1
+        }
 
+        function end(params) {
+            finished = true;
+            scoreBonus();
+            $("#response").html(null);
+            var highscore = prompt("Please enter your initials") + (` ${score}`)
+            i = localStorage.length+1;
+            localStorage.setItem(i, highscore);
+            console.log(highscore)
+            console.log(localStorage)
+            console.log(localStorage.length)
+        }
 
 
 
